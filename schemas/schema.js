f7268f2907971f02e1f5c4ed32543fa141d1a530
Corey Ward <corey.atx@gmail.com>
2020-03-16 15:42:22 -0500

    Add initial Restaurant document schema

--- schemas/schema.js
+++ schemas/schema.js
@@ -1,16 +1,113 @@
-// First, we must import the schema creator
-import createSchema from 'part:@sanity/base/schema-creator'
+import createSchema from "part:@sanity/base/schema-creator"
+import schemaTypes from "all:part:@sanity/base/schema-type"
 
-// Then import schema types from any plugins that might expose them
-import schemaTypes from 'all:part:@sanity/base/schema-type'
-
-// Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
-  // We name our schema
-  name: 'default',
-  // Then proceed to concatenate our document type
-  // to the ones provided by any plugins that are installed
+  name: "default",
   types: schemaTypes.concat([
-    /* Your types here! */
-  ])
+    {
+      title: "Restaurant",
+      name: "restaurant",
+      type: "document",
+      fieldsets: [
+        { title: "General Information", name: "information" },
+        { title: "Operating Policies", name: "policies" },
+        { title: "Ordering Information", name: "ordering" },
+      ],
+      fields: [
+        {
+          title: "Name",
+          name: "title",
+          type: "string",
+          validation: Rule => Rule.required(),
+          fieldset: "information",
+        },
+        {
+          title: "Website",
+          name: "website",
+          type: "url",
+          validation: Rule => Rule.uri(),
+          fieldset: "information",
+        },
+        {
+          title: "Menu URL",
+          name: "menuUrl",
+          type: "url",
+          validation: Rule => Rule.uri(),
+          fieldset: "information",
+        },
+        {
+          title: "Tags",
+          name: "tags",
+          type: "array",
+          of: [{ type: "string" }],
+          options: { layout: "tags" },
+        },
+        {
+          title: "Hours",
+          name: "hours",
+          type: "array",
+          of: [{ type: "string" }],
+          fieldset: "policies",
+        },
+        {
+          title: "Takeout Service Options",
+          name: "takeoutOptions",
+          description: "Leave blank if the restaurant is not offering takeout.",
+          type: "array",
+          of: [{ type: "string" }],
+          options: {
+            list: [
+              { title: "Offers Takeout", value: "takeout" },
+              { title: "Offers Curbside", value: "curbside" },
+              { title: "Delivery: Self-Delivered", value: "delivery" },
+              { title: "Delivery: Favor", value: "delivery-favor" },
+              { title: "Delivery: DoorDash", value: "delivery-doordash" },
+              { title: "Delivery: Postmates", value: "delivery-postmates" },
+              { title: "Delivery: GrubHub", value: "delivery-grubhub" },
+              { title: "Delivery: UberEats", value: "delivery-ubereats" },
+            ],
+          },
+          fieldset: "policies",
+        },
+        {
+          title: "Notes",
+          name: "policyNotes",
+          type: "text",
+          fieldset: "policies",
+        },
+        {
+          title: "Order Online URL",
+          name: "orderUrl",
+          type: "url",
+          validation: Rule => Rule.uri(),
+          fieldset: "ordering",
+        },
+        {
+          title: "Phone Number for Ordering",
+          name: "orderPhone",
+          type: "string",
+          fieldset: "ordering",
+        },
+        {
+          title: "Notes",
+          name: "orderingNotes",
+          type: "text",
+          fieldset: "ordering",
+        },
+        {
+          title: "Source URLs",
+          name: "sourceUrls",
+          description: "Links to sources for this information, when available.",
+          type: "array",
+          of: [{ type: "url", validation: Rule => Rule.required().uri() }],
+        },
+        {
+          title: "Last Update/Confirmation of Information",
+          name: "confirmedAt",
+          type: "datetime",
+          validation: Rule => Rule.required().max(new Date().toISOString()),
+        },
+      ],
+    },
+  ]),
 })
