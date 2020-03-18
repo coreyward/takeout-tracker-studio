721afc2194379f8cd2546bf59e542b45dd97d7b0
Corey Ward <corey.atx@gmail.com>
2020-03-18 12:50:22 -0500

    Add restaurant sublists

--- deskStructure.js
+++ deskStructure.js
@@ -1,8 +1,23 @@
 import S from "@sanity/desk-tool/structure-builder"
 import RestaurantIcon from "react-icons/lib/md/restaurant"
+import SublistIcon from "react-icons/lib/md/subdirectory-arrow-right"
 import ContactsIcon from "react-icons/lib/ti/contacts"
 import AnnouncementIcon from "react-icons/lib/fa/bullhorn"
 import DraftsIcon from "react-icons/lib/fa/pencil"
+import TagsIcon from "react-icons/lib/fa/tags"
+import ClockIcon from "react-icons/lib/fa/clock-o"
+import AuditIcon from "react-icons/lib/md/rate-review"
+import SadIcon from "react-icons/lib/io/sad"
+import moment from "moment"
+
+const sublist = (title, conditions) =>
+  S.listItem()
+    .title(title)
+    .child(
+      S.documentList()
+        .title(title)
+        .filter(conditions)
+    )
 
 export default () =>
   S.list()
@@ -11,6 +26,37 @@ export default () =>
       S.documentTypeListItem("restaurant")
         .title("Restaurants")
         .icon(RestaurantIcon),
+      S.listItem()
+        .title("Restaurant Sublists")
+        .icon(SublistIcon)
+        .child(
+          S.list()
+            .title("Restaurant Sublists")
+            .items([
+              sublist(
+                "Unverified / User Reported Only",
+                "_type == 'restaurant' && unverified == true"
+              ).icon(AuditIcon),
+              sublist(
+                "Closed for Business",
+                "_type == 'restaurant' && closedForBusiness == true"
+              ).icon(SadIcon),
+              sublist(
+                "Needs Tags",
+                "_type == 'restaurant' && !defined(tags)"
+              ).icon(TagsIcon),
+              sublist(
+                "Outdated / Needs Confirmation",
+                [
+                  "_type == 'restaurant'",
+                  `confirmedAt < "${moment()
+                    .subtract(3, "days")
+                    .toISOString()}"`,
+                  "closedForBusiness == false",
+                ].join(" && ")
+              ).icon(ClockIcon),
+            ])
+        ),
       S.divider(),
       S.documentTypeListItem("announcement")
         .title("Announcements")
