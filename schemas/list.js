5f4fb8b3d44c1fea1b108de3c759b15d30643096
Corey Ward <corey.atx@gmail.com>
2020-03-23 14:33:23 -0500

    Add fields for hero to list type

--- schemas/list.js
+++ schemas/list.js
@@ -1,7 +1,19 @@
+import hero from "./objects/hero"
+
 const list = {
   title: "List",
   name: "list",
   type: "document",
+  fieldsets: [
+    {
+      title: "Hero",
+      name: "hero",
+      options: {
+        collapsible: true,
+        collapsed: false,
+      },
+    },
+  ],
   fields: [
     {
       title: "Name",
@@ -9,11 +21,13 @@ const list = {
       type: "string",
       validation: Rule => Rule.required().max(50),
     },
-    {
-      title: "Description",
-      name: "description",
-      type: "text",
-    },
+
+    ...hero.fields
+      .filter(({ name }) => name !== "title")
+      .map(field => ({
+        ...field,
+        fieldset: "hero",
+      })),
     {
       title: "Restaurants",
       name: "restaurants",
@@ -21,6 +35,13 @@ const list = {
       of: [{ type: "reference", to: [{ type: "restaurant" }] }],
     },
   ],
+  preview: {
+    select: {
+      title: "name",
+      subtitle: "description",
+      media: "background",
+    },
+  },
 }
 
 export default list
